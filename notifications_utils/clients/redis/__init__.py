9d4336b7578c4a5b16fe9979015d3f6ac6d83aa4
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-04-19 08:18:49 -0400

    Add Redis keys when near or over the daily limit (#76)

--- notifications_utils/clients/redis/__init__.py
+++ notifications_utils/clients/redis/__init__.py
@@ -5,5 +5,13 @@ def daily_limit_cache_key(service_id):
     return "{}-{}-{}".format(str(service_id), datetime.utcnow().strftime("%Y-%m-%d"), "count")
 
 
+def near_daily_limit_cache_key(service_id):
+    return f"nearing-{daily_limit_cache_key(service_id)}"
+
+
+def over_daily_limit_cache_key(service_id):
+    return f"over-{daily_limit_cache_key(service_id)}"
+
+
 def rate_limit_cache_key(service_id, api_key_type):
     return "{}-{}".format(str(service_id), api_key_type)
--- notifications_utils/version.py
+++ notifications_utils/version.py
@@ -1,2 +1,2 @@
-__version__ = '43.4.0'
+__version__ = '43.5.0'
 # GDS version '34.0.1'
