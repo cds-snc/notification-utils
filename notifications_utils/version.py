238d3681940f7d06ac5e80d5dc6811fdb90b1d7b
emilywu-tw <ewu@thoughtworks.com>
2021-08-12 16:14:33 -0400

    [#24] Fix failing tests and bump version to 1.0.35

--- notifications_utils/version.py
+++ notifications_utils/version.py
@@ -1,3 +1,3 @@
-__version__ = '1.0.34'
+__version__ = '1.0.35'
 # GDS version '34.0.1'
 # CDS version '41.0.0'
--- tests/test_template_types.py
+++ tests/test_template_types.py
@@ -863,11 +863,11 @@ def test_subject_line_gets_replaced():
         mock.call('content', {}, html='passthrough', markdown_lists=True)
     ]),
     (HTMLEmailTemplate, {}, [
-        mock.call('content', {}, html='escape', markdown_lists=True, redact_missing_personalisation=False),
-        mock.call('content', {}, highlight_placeholder_text=False, html='escape', markdown_lists=True),
+        mock.call('content', {}, highlight_placeholder_text=False, html='escape', markdown_lists=True, redact_missing_personalisation=False),
+        mock.call('content', {}, html='escape', markdown_lists=True),
     ]),
     (EmailPreviewTemplate, {}, [
-        mock.call('content', {}, html='escape', markdown_lists=True, redact_missing_personalisation=False),
+        mock.call('content', {}, highlight_placeholder_text=False, html='escape', markdown_lists=True, redact_missing_personalisation=False),
         mock.call('subject', {}, html='escape', redact_missing_personalisation=False),
         mock.call('((email address))', {}, with_brackets=False),
     ]),
@@ -915,7 +915,7 @@ def test_subject_line_gets_replaced():
         mock.call('content', {}, html='passthrough', redact_missing_personalisation=True, markdown_lists=True),
     ]),
     (EmailPreviewTemplate, {'redact_missing_personalisation': True}, [
-        mock.call('content', {}, html='escape', markdown_lists=True, redact_missing_personalisation=True),
+        mock.call('content', {}, highlight_placeholder_text=False, html='escape', markdown_lists=True, redact_missing_personalisation=True),
         mock.call('subject', {}, html='escape', redact_missing_personalisation=True),
         mock.call('((email address))', {}, with_brackets=False),
     ]),
