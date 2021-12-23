fa5ca2a2418ae1a1986d1a9ed3814d0d25e719b3
Jimmy Royer <jimleroyer@gmail.com>
2021-12-23 14:32:27 -0500

    Fixed location of the waffle python script in docker entry point

--- .github/actions/waffles/entrypoint.sh
+++ .github/actions/waffles/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python waffles.py iron --app-loc $1 --app-lib $2 --flask-mod $3 --flask-prop $4 --base-url $5
+python /app/waffles.py iron --app-loc $1 --app-lib $2 --flask-mod $3 --flask-prop $4 --base-url $5
