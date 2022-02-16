4af538d1af22a76a0fb1d7bc25339deaaba4fee4
Erwan Jossic <erwan.jossic@mcn.gouv.qc.ca>
2022-02-16 15:27:12 -0500

    Ajout d'utilitaires pour faciliter le TDD : pytest-watch et @pytest.mark.focus

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,3 +28,33 @@ def sample_service():
 def rmock():
     with requests_mock.mock() as rmock:
         yield rmock
+
+# https://superorbital.io/journal/focusing-on-pytest/
+def pytest_configure(config):
+    """
+    Register the `focus` marker, so we don't get warnings.
+    """
+
+    config.addinivalue_line("markers", "focus: Only run this test.")
+
+
+def pytest_collection_modifyitems(items, config):
+    """
+    Focus on tests marked focus, if any.  Run all otherwise.
+    """
+
+    selected_items = []
+    deselected_items = []
+
+    focused = False
+    for item in items:
+        if item.get_closest_marker("focus"):
+            focused = True
+            selected_items.append(item)
+        else:
+            deselected_items.append(item)
+
+    if focused:
+        print("\nOnly running @pytest.mark.focus tests")
+        config.hook.pytest_deselected(items=deselected_items)
+        items[:] = selected_items
