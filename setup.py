390f9ef73b36a48d44f123ed3b186662c5359894
iokpala <ikenna.okpala@cds-snc.ca>
2022-05-17 12:08:46 -0300

    UPGRADE: Jinja2>=2.11.3
    This upgrade has been done in partial fullfilment for the upgrade of
    notifications-api

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "Flask>=0.12.2",
         "orderedset==2.0.3",
         "markupsafe==2.0.1",
-        "Jinja2==2.11.3",
+        "Jinja2>=2.11.3",
         "statsd==3.3.0",
         "Flask-Redis==0.4.0",
         "PyYAML==5.4.1",
