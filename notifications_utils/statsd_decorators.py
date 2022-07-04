a1cce77737e29ba4622a5aae671b31cb001309ff
Andrew Leith <andrew.leith@cds-snc.ca>
2022-07-04 11:13:21 +0000

    chore(mypy): remove unnecessary ignores

--- notifications_utils/statsd_decorators.py
+++ notifications_utils/statsd_decorators.py
@@ -13,10 +13,8 @@ def statsd(namespace):
             try:
                 res = func(*args, **kwargs)
                 elapsed_time = monotonic() - start_time
-                current_app.statsd_client.incr(  # type: ignore
-                    "{namespace}.{func}".format(namespace=namespace, func=func.__name__)
-                )
-                current_app.statsd_client.timing(  # type: ignore
+                current_app.statsd_client.incr("{namespace}.{func}".format(namespace=namespace, func=func.__name__))
+                current_app.statsd_client.timing(
                     "{namespace}.{func}".format(namespace=namespace, func=func.__name__), elapsed_time
                 )
 
@@ -70,7 +68,7 @@ def statsd_catch(namespace: str, counter_name: str, exception: Type[Exception]):
             try:
                 return func(*args, **kwargs)
             except exception as e:
-                current_app.statsd_client.incr(f"{namespace}.{counter_name}")  # type: ignore
+                current_app.statsd_client.incr(f"{namespace}.{counter_name}")
                 raise e
 
         wrapper.__wrapped__.__name__ = func.__name__  # type: ignore
