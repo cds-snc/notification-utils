58256fa03e8a9ffef33ae4b2d82613aa891f617b
Andrew <andrew.leith@cds-snc.ca>
2022-07-11 18:06:25 -0300

    Bump flask to v2 (#124)
    * Bump flask to v2, which bumps jinja and itsdangerous.
    
    * Ignores related to statsd.

--- notifications_utils/statsd_decorators.py
+++ notifications_utils/statsd_decorators.py
@@ -13,8 +13,10 @@ def statsd(namespace):
             try:
                 res = func(*args, **kwargs)
                 elapsed_time = monotonic() - start_time
-                current_app.statsd_client.incr("{namespace}.{func}".format(namespace=namespace, func=func.__name__))
-                current_app.statsd_client.timing(
+                current_app.statsd_client.incr(  # type: ignore
+                    "{namespace}.{func}".format(namespace=namespace, func=func.__name__)
+                )
+                current_app.statsd_client.timing(  # type: ignore
                     "{namespace}.{func}".format(namespace=namespace, func=func.__name__), elapsed_time
                 )
 
@@ -68,7 +70,7 @@ def statsd_catch(namespace: str, counter_name: str, exception: Type[Exception]):
             try:
                 return func(*args, **kwargs)
             except exception as e:
-                current_app.statsd_client.incr(f"{namespace}.{counter_name}")
+                current_app.statsd_client.incr(f"{namespace}.{counter_name}")  # type: ignore
                 raise e
 
         wrapper.__wrapped__.__name__ = func.__name__  # type: ignore
