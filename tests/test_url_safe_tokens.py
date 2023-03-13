71f52edbd141ac0416ab79f22a1a654170dce2b1
Stephen Astels <sastels@gmail.com>
2023-03-13 16:30:47 -0400

    use pytest.raises

--- tests/test_url_safe_tokens.py
+++ tests/test_url_safe_tokens.py
@@ -1,7 +1,7 @@
 import urllib
 
-from itsdangerous import BadSignature, BadTimeSignature, SignatureExpired
-from pytest import fail
+from itsdangerous import BadSignature, SignatureExpired
+import pytest
 
 from notifications_utils.url_safe_token import generate_token, check_token
 
@@ -17,11 +17,8 @@ def test_fails_for_incorrect_salt():
     payload = "email@something.com"
     token = generate_token(payload, "secret-key")
     token = urllib.parse.unquote(token)
-    try:
+    with pytest.raises(BadSignature):
         assert payload == check_token(token, "secret-key", salt="wrong-salt")
-        fail("Expected a BadTimeSignature exception")
-    except BadTimeSignature:
-        pass
 
 
 def test_should_return_payload_from_signed_token():
@@ -35,11 +32,8 @@ def test_should_throw_exception_when_token_is_tampered_with():
     import uuid
 
     token = generate_token(str(uuid.uuid4()), "secret-key", "dangerous-salt")
-    try:
+    with pytest.raises(BadSignature):
         check_token(token + "qerqwer", "secret-key", "dangerous-salt", 30)
-        fail()
-    except BadSignature:
-        pass
 
 
 def test_return_none_when_token_is_expired():
@@ -47,8 +41,5 @@ def test_return_none_when_token_is_expired():
     payload = "some_payload"
     token = generate_token(payload, "secret-key", "dangerous-salt")
     token = urllib.parse.unquote(token)
-    try:
+    with pytest.raises(SignatureExpired):
         assert check_token(token, "secret-key", "dangerous-salt", max_age) is None
-        fail("Expected a SignatureExpired exception")
-    except SignatureExpired:
-        pass
