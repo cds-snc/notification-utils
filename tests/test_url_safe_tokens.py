ceff3f03bd1cf4fc0c5b7246a39f21e769302af8
Steve Astels <sastels@users.noreply.github.com>
2023-03-21 14:48:39 -0400

    Make salt optional for tokens (#174)

--- tests/test_url_safe_tokens.py
+++ tests/test_url_safe_tokens.py
@@ -1,9 +1,24 @@
 import urllib
 
 from itsdangerous import BadSignature, SignatureExpired
-from pytest import fail
+import pytest
 
 from notifications_utils.url_safe_token import generate_token, check_token
+
+
+def test_works_without_salt():
+    payload = "email@something.com"
+    token = generate_token(payload, "secret-key")
+    token = urllib.parse.unquote(token)
+    assert payload == check_token(token, "secret-key")
+
+
+def test_fails_for_incorrect_salt():
+    payload = "email@something.com"
+    token = generate_token(payload, "secret-key")
+    token = urllib.parse.unquote(token)
+    with pytest.raises(BadSignature):
+        assert payload == check_token(token, "secret-key", salt="wrong-salt")
 
 
 def test_should_return_payload_from_signed_token():
@@ -17,11 +32,8 @@ def test_should_throw_exception_when_token_is_tampered_with():
     import uuid
 
     token = generate_token(str(uuid.uuid4()), "secret-key", "dangerous-salt")
-    try:
+    with pytest.raises(BadSignature):
         check_token(token + "qerqwer", "secret-key", "dangerous-salt", 30)
-        fail()
-    except BadSignature:
-        pass
 
 
 def test_return_none_when_token_is_expired():
@@ -29,8 +41,5 @@ def test_return_none_when_token_is_expired():
     payload = "some_payload"
     token = generate_token(payload, "secret-key", "dangerous-salt")
     token = urllib.parse.unquote(token)
-    try:
+    with pytest.raises(SignatureExpired):
         assert check_token(token, "secret-key", "dangerous-salt", max_age) is None
-        fail("Expected a SignatureExpired exception")
-    except SignatureExpired:
-        pass
