6785c4eee98ad7d2cfc495c7c6fdbad42625c3c7
Jimmy Royer <jimleroyer@gmail.com>
2023-04-13 17:52:14 -0400

    Fixed tests for recent expectation changes

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -342,11 +342,17 @@ def test_hrule(markdown_function, expected):
 
 
 @pytest.mark.parametrize(
-    "markdown_function, expected",
+    "markdown_function, markdown_input, expected",
     (
-        [notify_letter_preview_markdown, ("<ol>\n" "<li>one</li>\n" "<li>two</li>\n" "<li>three</li>\n" "</ol>\n")],
+        [
+            notify_letter_preview_markdown,
+            "1. one\n" "2. two\n" "3. three\n",
+            ("<ol>\n" "<li>one</li>\n" "<li>two</li>\n" "<li>three</li>\n" "</ol>\n"),
+        ],
+        [notify_letter_preview_markdown, "1.one\n" "2.two\n" "3.three\n", "<p>1.one<br>2.two<br>3.three</p>"],
         [
             notify_email_markdown,
+            "1. one\n" "2. two\n" "3. three\n",
             (
                 '<table role="presentation" style="padding: 0 0 20px 0;">'
                 "<tr>"
@@ -365,14 +371,24 @@ def test_hrule(markdown_function, expected):
             ),
         ],
         [
+            notify_email_markdown,
+            "1.one\n" "2.two\n" "3.three\n",
+            '<p style="Margin: 0 0 20px 0; font-size: 19px; line-height: 25px; color: #0B0C0C;">1.one<br />2.two<br />3.three</p>',
+        ],
+        [
             notify_plain_text_email_markdown,
+            "1. one\n" "2. two\n" "3. three\n",
             ("\n" "\n1. one" "\n2. two" "\n3. three"),
+        ],
+        [
+            notify_plain_text_email_markdown,
+            "1.one\n" "2.two\n" "3.three\n",
+            "\n\n1.one\n2.two\n3.three",
         ],
     ),
 )
-def test_ordered_list(markdown_function, expected):
-    assert markdown_function("1. one\n" "2. two\n" "3. three\n") == expected
-    assert markdown_function("1.one\n" "2.two\n" "3.three\n") == expected
+def test_ordered_list(markdown_function, markdown_input, expected):
+    assert markdown_function(markdown_input) == expected
 
 
 @pytest.mark.parametrize(
