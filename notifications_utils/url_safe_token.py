4076714caffbbe3fb08a75fe84d61fb9612d6d6e
Stephen Astels <sastels@gmail.com>
2023-05-10 13:23:31 -0400

    improve typing and documentation

--- notifications_utils/url_safe_token.py
+++ notifications_utils/url_safe_token.py
@@ -1,22 +1,50 @@
 from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
 from notifications_utils.formatters import url_encode_full_stops
+from typing import Any, List
 
 
-# currently api sends in a salt, we can stop doing that, and then remove the salt from the generate_token()
-def generate_token(payload, secret, salt="token"):
+def generate_token(payload: Any, secret: str | List[str], salt: str = "token") -> str:
+    """Create a signed token containing a payload. The token contains the creation time.
+
+    Note: After admin and api no longer pass a salt we can remove the salt parameter from this function
+    and simplify the tests of these functions
+
+    Parameters:
+    payload: The payload to be signed
+    secret: The secret to sign the payload with
+    salt: The salt that is passed in but no longer used
+
+    Returns:
+    A signed token containing the payload
+
+    """
     return url_encode_full_stops(URLSafeTimedSerializer(secret).dumps(payload, "token"))
 
 
-# After two days all valid tokens will be signed with the salt "token" instead of DANGEROUS_SALT.
-# So we can:
-#  - stop passing the salt to check_token() in api
-#  - remove the "try / except" block below (and just return the ser.loads())
-#  - tweak the tests to not pass in the salt
-def check_token(token, secret, salt="token", max_age_seconds=60 * 60 * 24):
+def check_token(token: str, secret: str | List[str], salt: str = "token", max_age_seconds: int = 60 * 60 * 24) -> Any:
+    """
+    Check that a token is valid and return the payload.
+
+    Note: We currently verify with either the passed in salt or the salt "token".
+    After admin and api no longer pass a salt we can remove the salt parameter from this function
+    and change the "try / except" block to a simple "return ser.loads(..)"
+
+    Parameters:
+    token: The token to be checked
+    secret: The secret to check the token with
+    salt: The salt that is passed in and may be used to check the token
+    max_age_seconds: The maximum age of the token in seconds
+
+    Returns:
+    The payload of the token if it is not too old and is valid
+    raises SignatureExpired if the token is too old
+    raises BadSignature if the token is invalid
+
+    """
     ser = URLSafeTimedSerializer(secret)
     try:
         return ser.loads(token, max_age=max_age_seconds, salt="token")
-    except SignatureExpired:
+    except SignatureExpired:  # SignatureExpired is a subclass of BadSignature so we ensure it is raised first
         raise
     except BadSignature:
         return ser.loads(token, max_age=max_age_seconds, salt=salt)
