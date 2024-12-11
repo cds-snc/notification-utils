b3085f8686c676c914853d413601295c9dd91a5b
Andrew <andrew.leith@cds-snc.ca>
2024-12-11 11:26:05 -0400

    Feat/rtl 2 (#340)
    * feat(rtl): add rtl feature within template language
    
    * test(rtl): add tests!
    
    * chore: bump version

--- tests/test_template.py
+++ tests/test_template.py
@@ -38,3 +38,92 @@ def test_lang_tags_in_templates_bad_content(bad_content: str):
 def test_lang_tags_in_templates_good_content(good_content: str):
     html = get_html_email_body(good_content, {})
     assert '<div lang="fr-ca">' in html
+
+
+class TestRTLTags:
+    def test_rtl_tags_in_templates(self):
+        content = "[[rtl]]\nRTL content\n[[/rtl]]"
+        html = get_html_email_body(content, {})
+        assert '<div dir="rtl">' in html
+        assert "RTL content" in html
+
+    @pytest.mark.parametrize(
+        "nested_content",
+        [
+            "[[rtl]]\nRTL content\n[[/rtl]]\n[[rtl]]\nMore RTL content\n[[/rtl]]",
+            "[[rtl]]\nRTL content with [[en]]\nEN content\n[[/en]]\n[[/rtl]]",
+        ],
+    )
+    def test_rtl_tags_in_templates_nested_content(self, nested_content: str):
+        html = get_html_email_body(nested_content, {})
+        assert '<div dir="rtl">' in html
+        assert "RTL content" in html
+
+    @pytest.mark.parametrize(
+        "bad_content",
+        [
+            "[[rtl]\nRTL content\n[[/rtl]]",  # missing bracket
+            "[[RTL]]\nRTL content\n[[/RTL]]",  # tags not lowercase
+            "[[rtl]]\nRTL content\n",  # tag missing
+            "RTL content\n[[/rtl]]",  # tag missing
+            "((rtl))\nRTL content\n((/rtl))",  # wrong brackets
+        ],
+    )
+    def test_rtl_tags_in_templates_bad_content(self, bad_content: str):
+        html = get_html_email_body(bad_content, {})
+        assert '<div dir="rtl">' not in html
+
+    @pytest.mark.parametrize(
+        "mixed_content",
+        [
+            "[[rtl]]\nRTL content\n[[/rtl]]\nLTR content",
+            "LTR content\n[[rtl]]\nRTL content\n[[/rtl]]",
+        ],
+    )
+    def test_rtl_tags_in_templates_mixed_content(self, mixed_content: str):
+        html = get_html_email_body(mixed_content, {})
+        assert '<div dir="rtl">' in html
+        assert "RTL content" in html
+        assert "LTR content" in html
+
+    @pytest.mark.parametrize(
+        "content, extra_tag",
+        [
+            ("[[rtl]] # RTL CONTENT [[/rtl]]", "h2"),
+            ("[[rtl]] ## RTL CONTENT [[/rtl]]", "h3"),
+            ("[[rtl]]\n- RTL CONTENT 1\n-item 2\n[[/rtl]]", "ul"),
+            ("[[rtl]]\n1. RTL CONTENT 1\n1. item 2\n[[/rtl]]", "ol"),
+            ("[[rtl]]**RTL CONTENT**[[/rtl]]", "strong"),
+            ("[[rtl]]_RTL CONTENT_[[/rtl]]", "em"),
+            ("[[rtl]]---\nRTL CONTENT[[/rtl]]", "hr"),
+            (
+                "[[rtl]]1. RTL CONTENT\n1. First level\n   1. Second level\n   1. Second level\n   1. Second level\n      1. Third level\n      1. Third level\n         1. Fourth level\n         1. Fourth level\n            1. Fifth level\n            1. Fifth level[[/rtl]]",
+                "ol",
+            ),
+            ("[[rtl]]^RTL CONTENT[[/rtl]]", "blockquote"),
+            ("[[rtl]]RTL CONTENT now at https://www.canada.ca[[/rtl]]", "a"),
+            ("[[rtl]][RTL CONTENT](https://www.canada.ca/sign-in)[[/rtl]]", "a"),
+            ("[[rtl]][[en]]RTL CONTENT[[/en]][[/rtl]]", 'div lang="en-ca"'),
+            ("[[rtl]][[fr]]RTL CONTENT[[/fr]][[/rtl]]", 'div lang="fr-ca"'),
+        ],
+        ids=[
+            "heading_1",
+            "heading_2",
+            "list_unordered",
+            "list_ordered",
+            "bold",
+            "italic",
+            "hr",
+            "nested_list",
+            "blockquote",
+            "link",
+            "link_with_text",
+            "nested_lang_tags_en",
+            "nested_lang_tags_fr",
+        ],
+    )
+    def test_rtl_tags_work_with_other_features(self, content: str, extra_tag: str):
+        html = get_html_email_body(content, {})
+        assert '<div dir="rtl">' in html
+        assert "RTL CONTENT" in html
+        assert "<{}".format(extra_tag) in html
