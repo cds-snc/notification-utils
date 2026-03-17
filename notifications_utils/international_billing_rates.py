9740f3ee4338a116c2168c51808090b212c4d63e
Stephen McMurtry <smcmurtry@gmail.com>
2026-03-17 18:53:18 +0000

    add a script to output a csv file with the international rates

--- notifications_utils/international_billing_rates.py
+++ notifications_utils/international_billing_rates.py
@@ -18,6 +18,7 @@ Format of the yaml file looks like:
   - Dominican Republic
 """
 
+import csv
 import os
 
 import yaml
@@ -27,3 +28,27 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 with open("{}/international_billing_rates.yml".format(dir_path)) as f:
     INTERNATIONAL_BILLING_RATES = yaml.safe_load(f)
     COUNTRY_PREFIXES = list(reversed(sorted(INTERNATIONAL_BILLING_RATES.keys(), key=len)))
+
+
+def export_multipliers_csv(output_path="international_billing_rates_multipliers.csv"):
+    """
+    Write a CSV file containing the billing multipliers for all countries
+    that can be sent to (i.e. where can_send is true).
+
+    Columns: country, country_code, rate_multiplier
+    """
+    rows = []
+    for prefix, entry in INTERNATIONAL_BILLING_RATES.items():
+        if entry.get("attributes", {}).get("can_send"):
+            multiplier = entry["billable_units"]
+            for country in entry.get("names", []):
+                rows.append({"country": country, "country_code": prefix, "rate_multiplier": multiplier})
+
+    rows.sort(key=lambda r: r["country"])
+
+    with open(output_path, "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=["country", "country_code", "rate_multiplier"])
+        writer.writeheader()
+        writer.writerows(rows)
+
+    return output_path
